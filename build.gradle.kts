298695e6477f4f8d19a5db026cd45175f41f861f
T45K <tasktas9@gmail.com>
2025-10-28 00:20:03 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,9 @@ dependencies {
     // Kotest 6
     testImplementation("io.kotest:kotest-runner-junit5:6.0.4")
     testImplementation("io.kotest:kotest-assertions-core:6.0.4")
-    testImplementation("io.kotest:kotest-extensions-spring:6.0.4")
-    // testImplementation("io.kotest:kotest-extensions-spring:6.0.3")
+    testImplementation("io.kotest:kotest-extensions-spring:6.0.4") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-web")
+    }
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
